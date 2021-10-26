948a2682eb06d57297b98b79b3631c4004a7140e
HamdiNawfel <hamdii.nawfel@gmail.com>
2021-10-26 01:14:25 +0100

    build mongodb service and config the connection

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
+
 const app = express();
-const port = process.env.PORT || 8080;
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("succesfully connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
-  res.send("Hello AQQQ");
+  res.send("Hello work fine!!!");
 });
-
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`app listening at http://localhost:${port}`);
 });
